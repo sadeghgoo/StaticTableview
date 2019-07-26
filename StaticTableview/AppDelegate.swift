f3aa813728c279b3a2f513f5e191c2fc32d4e2b3
sadeghgoo <sadeghitunes2@gmail.com>
2019-07-26 15:41:27 +0430

    image added

--- StaticTableview/AppDelegate.swift
+++ StaticTableview/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "TableViewController")
+        
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
